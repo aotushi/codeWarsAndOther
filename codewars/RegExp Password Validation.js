b5e436a344e46059c91a49a3ead50a92f5777c1c
sh <aotushi@hotmail.com>
2022-03-16 13:34:41 +0800

    feat: RegExp password validation

--- codewars/RegExp Password Validation.js
+++ codewars/RegExp Password Validation.js
@@ -6,3 +6,19 @@
  *  
  */
 
+const validate = psd => {
+  return /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])^[0-9a-zA-Z]{6,}$/.test(psd);
+}
+
+// other recommend solution
+
+function validate2(psd) {
+  return  /^[0-9a-zA-Z]{6,}$/.test(psd) &&
+          /[a-z]+/           .test(psd) &&
+          /[A-Z]+/           .test(psd) &&
+          /[0-9]+/           .test(psd);            
+}
+
+function validate3(psd) {
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{6,}$/.test(psd);
+}
